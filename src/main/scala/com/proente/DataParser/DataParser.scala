1a226fe4d3326f1a1becde17ab610d7f0d720894
Safa Keskin <keskinsaf@gmail.com>
2019-09-17 15:23:53 +0300

    fix(Helper): change tables to all tables

--- src/main/scala/com/proente/DataParser/DataParser.scala
+++ src/main/scala/com/proente/DataParser/DataParser.scala
@@ -6,7 +6,7 @@ import org.apache.spark.sql.{SparkSession, DataFrame}
 
 object DataParser extends SparkSessionBuilder {
   val dataBaseDir = "/home/safa/Proente/Files/ScriptExports"
-  val tables = Seq("shots")
+  val tables = Seq("shots", "defects", "failures", "machine_actions", "off_grades", "shots")
 
   override def run(spark: SparkSession): Unit = {
     tables.foreach( table => {
